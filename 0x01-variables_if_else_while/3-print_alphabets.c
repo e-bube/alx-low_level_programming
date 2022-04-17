22f4097e17171ba69b6e0c67219d1c5fa1a4e743
e-bube <msgiftanyanwu@gmail.com>
2022-04-17 16:44:31 -0700

    fourth task

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success/correct)
+  */
+int main(void)
+{
+char lowerCase;
+char upperCase;
+
+for (upperCase = 'A'; upperCase <= 'Z'; upperCase++)
+{
+putchar(upperCase);
+}
+
+for (lowerCase = 'a'; lowerCase <= 'z'; lowerCase++)
+{
+putchar(lowerCase);
+}
+putchar('\n');
+return (0);
+}
