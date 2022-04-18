8dd63206341f6766dbf539dd772548bd4a6aac25
e-bube <msgiftanyanwu@gmail.com>
2022-04-18 03:34:50 -0700

    print all combinations of two digit numbers

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,16 +8,19 @@ int main(void)
 int d1;
 int d2;
 
-for (d1 = 48; d2 <= 78; d1++)
+for (d1 = '0'; d1 < '9'; d1++)
 {
-for (d2 = d1 + 1; d2 <= 78; d2++)
+for (d2 = d1 + 1; d2 <= '9'; d2++)
+{
+if (d1 != d2)
 {
 putchar(d1);
 putchar(d2);
 
-if ((d1 == 56) && (d2 == 78))
-break;
-
+if (d1 == '8' && d2 == '9')
+{
+continue;
+}
 putchar(',');
 putchar(' ');
 }
