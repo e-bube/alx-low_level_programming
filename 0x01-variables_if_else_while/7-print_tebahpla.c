cda49a6fce36417e45d32df2c53d27b7f85efaea
e-bube <msgiftanyanwu@gmail.com>
2022-04-17 17:52:03 -0700

    eight task

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  * Return: 0
+  */
+int main()
+{
+char alphabets;
+
+for (alphabets = 122 ; alpahbets >= 97; alphabets--)
+{
+putchar(alphabets);
+}
+putchar('\n');
+return (0);
+}
