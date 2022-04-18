ea9c7e1c3d9f3c8145309cd04bf6f533af772e58
e-bube <msgiftanyanwu@gmail.com>
2022-04-17 17:34:43 -0700

    seventh task

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  * Return: 0
+  */
+int main(void)
+{
+int numberz;
+
+for (numberz = 0 ; numberz < 10; numberz++)
+{
+putchar (numberz);
+}
+putchar ('\n');
+return (0);
+}
