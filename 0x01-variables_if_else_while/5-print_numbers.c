5089ba00df7484fd5263ee57d20c7b4c06e237a7
e-bube <msgiftanyanwu@gmail.com>
2022-04-17 17:15:09 -0700

    sixth task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  * Return: 0
+  */
+int main(void)
+{
+int wholeNumbers;
+for (wholeNumbers = 0 ; wholeNumbers < 10; wholeNumbers++)
+{
+printf("%d\n", wholeNumbers)'
+}
+return (0);
