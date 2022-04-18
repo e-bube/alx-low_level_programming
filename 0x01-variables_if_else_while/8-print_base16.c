88c2ad5183336e39ac06d34c52761b6ee05cd389
e-bube <msgiftanyanwu@gmail.com>
2022-04-17 18:09:56 -0700

    ninth task

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  * Return: 0
+  */
+int main(void)
+{
+char baseSixteen;
+
+for (baseSixteen = 48; baseSixteen <= 57; baseSixteen++)
+{
+putchar(baseSixteen);
+}
+for (baseSixteen = 97; baseSixteen <= 102; baseSixteen++)
+{
+putchar(baseSixteen);
+}
+putchar ('\n');
+return (0);
+}
